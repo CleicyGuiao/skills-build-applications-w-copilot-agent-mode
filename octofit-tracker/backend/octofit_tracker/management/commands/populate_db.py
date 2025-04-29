262a29365edae9e38315935cf5fcb683e9c50d16
Srta.Clei <74973981+CleicyGuiao@users.noreply.github.com>
2025-04-29 16:43:29 +0000

    agora vai

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -1,35 +1,69 @@
 from django.core.management.base import BaseCommand
 from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
+from django.conf import settings
+from pymongo import MongoClient
+from datetime import timedelta
+from bson import ObjectId
 from django.utils import timezone
 
 class Command(BaseCommand):
     help = 'Popula o banco octofit_db com dados de teste para users, teams, activity, leaderboard e workouts.'
 
     def handle(self, *args, **kwargs):
+        # Conecta ao MongoDB
+        client = MongoClient(settings.DATABASES['default']['CLIENT']['host'], int(settings.DATABASES['default']['CLIENT']['port']))
+        db = client[settings.DATABASES['default']['NAME']]
+
+        # Limpa as coleções
+        db.users.drop()
+        db.teams.drop()
+        db.activity.drop()
+        db.leaderboard.drop()
+        db.workouts.drop()
+
         # Usuários
-        user1 = User.objects.create(email='alice@example.com', name='Alice', password='senha123')
-        user2 = User.objects.create(email='bob@example.com', name='Bob', password='senha456')
-        user3 = User.objects.create(email='carol@example.com', name='Carol', password='senha789')
+        users = [
+            User(_id=ObjectId(), email='thundergod@mhigh.edu', name='thundergod', password='thundergodpassword'),
+            User(_id=ObjectId(), email='metalgeek@mhigh.edu', name='metalgeek', password='metalgeekpassword'),
+            User(_id=ObjectId(), email='zerocool@mhigh.edu', name='zerocool', password='zerocoolpassword'),
+            User(_id=ObjectId(), email='crashoverride@hmhigh.edu', name='crashoverride', password='crashoverridepassword'),
+            User(_id=ObjectId(), email='sleeptoken@mhigh.edu', name='sleeptoken', password='sleeptokenpassword'),
+        ]
+        User.objects.bulk_create(users)
 
         # Times
-        team1 = Team.objects.create(name='Team Octo')
-        team2 = Team.objects.create(name='Team Fit')
-        team1.members.add(user1, user2)
-        team2.members.add(user3)
+        blue_team = Team(_id=ObjectId(), name='Blue Team')
+        gold_team = Team(_id=ObjectId(), name='Gold Team')
+        blue_team.save()
+        gold_team.save()
+        blue_team.members.add(*users)
+        gold_team.members.add(*users)
 
         # Atividades
-        Activity.objects.create(user=user1, type='run', duration=30, date=timezone.now())
-        Activity.objects.create(user=user2, type='walk', duration=45, date=timezone.now())
-        Activity.objects.create(user=user3, type='strength', duration=20, date=timezone.now())
-
-        # Workouts
-        Workout.objects.create(user=user1, description='Pushups and situps', date=timezone.now())
-        Workout.objects.create(user=user2, description='Running intervals', date=timezone.now())
-        Workout.objects.create(user=user3, description='Yoga and stretching', date=timezone.now())
+        activities = [
+            Activity(_id=ObjectId(), user=users[0], type='Cycling', duration=60, date=timezone.now()),
+            Activity(_id=ObjectId(), user=users[1], type='Crossfit', duration=120, date=timezone.now()),
+            Activity(_id=ObjectId(), user=users[2], type='Running', duration=90, date=timezone.now()),
+            Activity(_id=ObjectId(), user=users[3], type='Strength', duration=30, date=timezone.now()),
+            Activity(_id=ObjectId(), user=users[4], type='Swimming', duration=75, date=timezone.now()),
+        ]
+        Activity.objects.bulk_create(activities)
 
         # Leaderboard
-        Leaderboard.objects.create(team=team1, points=150)
-        Leaderboard.objects.create(team=team2, points=100)
+        leaderboard_entries = [
+            Leaderboard(_id=ObjectId(), team=blue_team, points=100),
+            Leaderboard(_id=ObjectId(), team=gold_team, points=90),
+        ]
+        Leaderboard.objects.bulk_create(leaderboard_entries)
+
+        # Workouts
+        workouts = [
+            Workout(_id=ObjectId(), user=users[0], description='Cycling Training', date=timezone.now()),
+            Workout(_id=ObjectId(), user=users[1], description='Crossfit', date=timezone.now()),
+            Workout(_id=ObjectId(), user=users[2], description='Running Training', date=timezone.now()),
+            Workout(_id=ObjectId(), user=users[3], description='Strength Training', date=timezone.now()),
+            Workout(_id=ObjectId(), user=users[4], description='Swimming Training', date=timezone.now()),
+        ]
+        Workout.objects.bulk_create(workouts)
 
         self.stdout.write(self.style.SUCCESS('Dados de teste populados com sucesso!'))
-#teste
